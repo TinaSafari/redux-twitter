d811d66bb52231550e5b73ffd7b1fad05aa5f4b1
Tina Safari <tina.safari85@gmail.com>
2020-08-14 09:58:59 -0400

    add middleware and componenets

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {handleIntialData} from "../actions/shared";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
-  }
+    componentDidMount() {
+        this.props.dispatch(handleIntialData())
+    }
+
+    render() {
+        return (
+            <div>
+                Starter Code
+            </div>
+        )
+    }
 }
 
-export default App
+export default connect()(App)
