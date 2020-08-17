91f2feb793710195b0421830930cfe9ea7e052fb
Tina Safari <tina.safari85@gmail.com>
2020-08-17 12:33:09 -0400

    TOGGLE_TWEET

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {formatDate, formatTweet} from "../utils/helpers";
-import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
+import {TiArrowBackOutline, TiHeartFullOutline, TiHeartOutline} from 'react-icons/ti/index'
+import {handleToggleTweet} from "../actions/tweets";
 
 
 class Tweet extends Component {
     handleLike = (e) => {
         e.preventDefault()
 
-        // todo: Handle Like Tweet
+        const {dispatch, tweet, authedUser} = this.props
+
+        dispatch(handleToggleTweet({
+
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
     toParent = (e, id) => {
         e.preventDefault()
         // todo: Redirect to parent Tweet.
     }
-    render()
-    {
+
+    render() {
         const {tweet} = this.props
 
         if (tweet === null) {
